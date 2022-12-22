cf5d313b1149af8213cbb2b59bb4960c3011af40
Gabriel Zeus B. Reyes <gabrielzeusreyes@gmail.com>
2022-12-22 18:07:03 +0800

    feat: make neovim transparent

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,17 @@
-local colorscheme = "everforest"
+local ok_status, nightfox = pcall(require, "nightfox")
+if not ok_status then
+  return
+end
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+nightfox.setup {
+  options = {
+    transparent = true
+  }
+}
+
+local colorscheme = "carbonfox"
+
+local status_ok,_ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
