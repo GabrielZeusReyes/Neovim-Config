1761ebb0d80f66bcd7ccfa911f15bae9409cc533
Gabz Reyes <gabrielzeusreyes@gmail.com>
2023-01-06 03:55:59 +0800

    feat: update mode in lualine, revert progress

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,71 +1,81 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-	return
+  return
 end
 
 local hide_in_width = function()
-	return vim.fn.winwidth(0) > 80
+  return vim.fn.winwidth(0) > 80
 end
 
 local diagnostics = {
-	"diagnostics",
-	sources = { "nvim_diagnostic" },
-  separator = { right = "" },
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
   sections = { "error", "warn" },
-	symbols = { error = " ", warn = " " },
-	colored = false,
-	update_in_insert = false,
-	always_visible = true,
+  symbols = { error = " ", warn = " " },
+  colored = false,
+  update_in_insert = false,
+  always_visible = true,
 }
 
 local diff = {
-	"diff",
-	colored = false,
-	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+  "diff",
+  colored = false,
+  symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
   cond = hide_in_width
 }
 
 local mode = {
-	"mode",
-	--[[ fmt = function(str) ]]
-	--[[ 	return "-- " .. str .. " --" ]]
-	--[[ end, ]]
-  separator = { right = "" },
-	padding = { left = 2 },
+  "mode",
+  --[[ fmt = function(str) ]]
+  --[[ 	return "-- " .. str .. " --" ]]
+  --[[ end, ]]
+  fmt = function(str)
+    local firstLetter = str:sub(1, 1)
+    local modeIcons = {
+      I = " ",
+      N = " ",
+      C = " ",
+      V = " ",
+      R = " "
+    }
+    return modeIcons[firstLetter]
+  end,
+  separator = { left = "", right = "" },
 }
--- '' '' '' '' '' '' '' '' 
+-- '' '' '' '' '' '' '' ''
 
 local filetype = {
-	"filetype",
-	icons_enabled = true,
-	icon = nil,
+  "filetype",
+  icons_enabled = true,
+  icon = nil,
 }
 
 local branch = {
-	"branch",
-	icons_enabled = true,
-	icon = "",
-  separator = { left = "" }
+  "branch",
+  icons_enabled = true,
+  icon = "",
+  separator = { left = "", right = "" },
+  padding = { left = 2, right = 1 }
 }
 
 local location = {
-	"location",
-	padding = { left = 0, right = 2 },
+  "location",
+  padding = { left = 0, right = 2 },
   separator = { left = "" }
 }
 
 -- cool function for progress
 local progress = function()
-	local current_line = vim.fn.line(".")
-	local total_lines = vim.fn.line("$")
-	local chars = { '🌑 ', '🌒 ', '🌓 ', '🌔 ', '🌕 ', '🌖 ', '🌗 ', '🌘 ', '🌑 ' }
-	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
+  local current_line = vim.fn.line(".")
+  local total_lines = vim.fn.line("$")
+  local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+  local line_ratio = current_line / total_lines
+  local index = math.ceil(line_ratio * #chars)
+  return chars[index]
 end
 
 local spaces = function()
-	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
 
 -- colors *from tokyonight
@@ -82,8 +92,8 @@ local colors = {
 }
 
 lualine.setup({
-	options = {
-		icons_enabled = true,
+  options = {
+    icons_enabled = true,
     theme = {
       inactive = {
         a = { fg = colors.gray, bg = colors.outerbg, gui = "bold" },
@@ -116,36 +126,47 @@ lualine.setup({
         c = { fg = colors.gray, bg = colors.innerbg },
       },
     },
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-		disabled_filetypes = { "dashboard", "NvimTree", "Outline", "spectre_panel" },
-		always_divide_middle = true,
-	},
-	sections = {
-		lualine_a = { branch, diagnostics },
-		lualine_b = { mode },
-		lualine_c = {},
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { diff, spaces, "encoding", filetype },
-		lualine_y = { location },
-		lualine_z = { { progress, separator = { left = "", right = "" } } }
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = { "dashboard", "NvimTree", "Outline", "spectre_panel" },
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = { mode },
+    lualine_b = { branch },
+    lualine_c = { diagnostics },
+    -- lualine_x = { "encoding", "fileformat", "filetype" },
+    lualine_x = { diff, spaces, "encoding", filetype },
+    lualine_y = { location },
+    lualine_z = {
+      {
+        progress,
+        color = {
+          fg = colors.outerbg
+        },
+        separator = {
+          left = "",
+          right = ""
+        }
+      }
+    }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {
     lualine_a = {
-			{
-				"buffers",
-				separator = { left = "", right = "" },
-				right_padding = 2,
-				symbols = { alternate_file = "" },
-			},
-		},
+      {
+        "buffers",
+        separator = { left = "", right = "" },
+        right_padding = 2,
+        symbols = { alternate_file = "" },
+      },
+    },
   },
 })
