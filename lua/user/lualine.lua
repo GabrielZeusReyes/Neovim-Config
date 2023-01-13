a9cdf6389c8bb89fdc1b0796d1184aeb55280b32
Gabriel Zeus B. Reyes <gabrielzeusreyes@gmail.com>
2023-01-13 18:28:57 +0800

    feat: lualine update progress to use a clock emoji

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -114,9 +114,10 @@ local progress = function()
   local current_line = vim.fn.line(".")
   local total_lines = vim.fn.line("$")
   local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+  local clock = { "🕛", "🕐", "🕒", "🕓", "🕕", "🕖", "🕗", "🕘", "🕙", }
   local line_ratio = current_line / total_lines
-  local index = math.ceil(line_ratio * #chars)
-  return chars[index]
+  local index = math.ceil(line_ratio * #clock)
+  return clock[index]
 end
 
 local spaces = function()
