2e3ac8c5d9480679296e9e0bcc5565e3f234af0c
Gabriel Zeus B. Reyes <gabrielzeusreyes@gmail.com>
2023-02-27 15:22:07 +0800

    refactor: added conditional statements for using formatter in null_ls

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -8,12 +8,47 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 -- local diagnostics = null_ls.builtins.diagnostics
 
+local root_has_file = function(files)
+  return function(utils)
+    return utils.root_has_file(files)
+  end
+end
+
+local eslint_root_files = { ".eslintrc", ".eslintrc.js", ".eslintrc.json" }
+local prettier_root_files = { ".prettierrc", ".prettierrc.js", ".prettierrc.json" }
+local stylua_root_files = { "stylua.toml", ".stylua.toml" }
+local elm_root_files = { "elm.json" }
+
+local opts = {
+  eslint_formatting = {
+    condition = function(utils)
+      local has_eslint = root_has_file(eslint_root_files)(utils)
+      local has_prettier = root_has_file(prettier_root_files)(utils)
+      return has_eslint and not has_prettier
+    end,
+  },
+  eslint_diagnostics = {
+    condition = root_has_file(eslint_root_files),
+  },
+  prettier_formatting = {
+    condition = root_has_file(prettier_root_files),
+  },
+  stylua_formatting = {
+    condition = root_has_file(stylua_root_files),
+  },
+  elm_format_formatting = {
+    condition = root_has_file(elm_root_files),
+  },
+}
+
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }), -- not sure if this will override your .prettierrc file??
-		formatting.stylua,
-    -- diagnostics.eslint,
-    -- formatting.eslint
+    -- diagnostics.eslint_d.with(opts.eslint_diagnostics),
+    formatting.eslint_d.with(opts.eslint_formatting),
+		formatting.prettier.with(opts.prettier_formatting),
+    formatting.stylua.with(opts.stylua_formatting),
+    formatting.elm_format.with(opts.elm_format_formatting),
+    -- code_actions.eslint_d.with(opts.eslint_diagnostics),
 	},
 })
