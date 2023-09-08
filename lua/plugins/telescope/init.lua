4f5fc65c363b44d4f672c17cafb546b34a8e066f
Gabriel Zeus B. Reyes <gabrielzeusreyes@gmail.com>
2023-09-08 19:45:48 +0800

    refactor: remove ui-select, use 0.1.2 for telescope

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -1,17 +1,12 @@
 return {
   "nvim-telescope/telescope.nvim",
+  tag = '0.1.2',
   dependencies = {
     {
       'nvim-telescope/telescope-fzf-native.nvim', -- if this doesn't work, try commenting it out first, run Lazy, clean this package, then install it again
       build = 'make',
       config = function()
         require('telescope').load_extension('fzf')
-      end
-    },
-    {
-      'nvim-telescope/telescope-ui-select.nvim',
-      config = function()
-        require("telescope").load_extension("ui-select")
       end
     },
     {
